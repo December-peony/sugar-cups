215b9c73fa8f0c08aaef0c36772aabc6b8b3d5a9
Anne <esraa.khaled3012@gmail.com>
2019-12-19 03:04:19 +0200

    well

--- suger.c
+++ suger.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
 
+int main(int argc, char* argv[])
+{  if (argc > 1 )
+  {
+    check( char* argv[]);
+  }
+   else 
+  {  printf("You have to specify The material"); 
+  }
+   return 0;
+}
